4234e7d8117446be104408918bafeb270f7c5f24
vorkorro14 <vladimir.litvinenko.1997@gmail.com>
2017-04-05 19:32:29 +0300

    Debugging changes

--- main.c
+++ main.c
@@ -36,6 +36,36 @@ uint8_t mac_addr[6] = {0x00,0x13,0x37,0x01,0x23,0x45};
 uint32_t ip_addr = inet_addr(192,168,0,222);
 
 
+
+//PACKETS
+//
+
+// Ethernet-frame
+typedef struct eth_frame {
+	uint8_t to_addr[6]; // destination address
+	uint8_t from_addr[6]; //  source address
+	uint16_t type; // protocol
+	uint8_t data[];
+} eth_frame_t;
+
+// ARP-packet
+typedef struct arp_message {
+	uint16_t hw_type; // Link layer protocol (Ethernet)
+	uint16_t proto_type; // Internet layer protocol (IP)
+	uint8_t hw_addr_len; // MAC-address length =6
+	uint8_t proto_addr_len; // IP-address length =4
+	uint16_t type; // Message type (request/response)
+	uint8_t mac_addr_from[6]; // Source MAC-address
+	uint32_t ip_addr_from; // Source IP-address
+	uint8_t mac_addr_to[6]; // Destination MAC-address, zeros if unknown
+	uint32_t ip_addr_to; // Destination IP-address
+} arp_message_t;
+
+//
+//END OF PACKETS
+
+
+
 void eth_reply(eth_frame_t *frame, uint16_t len);
 void eth_filter(eth_frame_t *frame, uint16_t len);
 void arp_filter(eth_frame_t *frame, uint16_t len);
@@ -49,14 +79,6 @@ void ip_filter();
 
 #define ETH_TYPE_ARP        htons(0x0806)
 #define ETH_TYPE_IP            htons(0x0800)
-
-// Ethernet-frame
-typedef struct eth_frame {
-	uint8_t to_addr[6]; // destination address
-	uint8_t from_addr[6]; //  source address
-	uint16_t type; // protocol
-	uint8_t data[];
-} eth_frame_t;
 
 // Sending answer to Ethernet-frame
 void eth_reply(eth_frame_t *frame, uint16_t len)
@@ -85,4 +107,60 @@ void eth_filter(eth_frame_t *frame, uint16_t len)
 			break;
 		}
 	}
-}A
+}
+
+//
+//END OF ETHERNET
+
+
+
+//ARP
+//
+
+#define ARP_HW_TYPE_ETH        htons(0x0001)
+#define ARP_PROTO_TYPE_IP    htons(0x0800)
+
+#define ARP_TYPE_REQUEST    htons(1)
+#define ARP_TYPE_RESPONSE    htons(2)
+
+// ARP-packet handler
+void arp_filter(eth_frame_t *frame, uint16_t len)
+{
+	arp_message_t *msg = (void*)(frame->data);
+
+	// Checking size of frame
+	if(len >= sizeof(arp_message_t))
+	{
+		// Checking protocol type
+		if( (msg->hw_type == ARP_HW_TYPE_ETH) &&
+		(msg->proto_type == ARP_PROTO_TYPE_IP) )
+		{
+			// ARP-request and self IP-address?
+			if( (msg->type == ARP_TYPE_REQUEST) &&
+			(msg->ip_addr_to == ip_addr) )
+			{
+				// Sending an answer
+				msg->type = ARP_TYPE_RESPONSE;
+				memcpy(msg->mac_addr_to, msg->mac_addr_from, 6);
+				memcpy(msg->mac_addr_from, mac_addr, 6);
+				msg->ip_addr_to = msg->ip_addr_from;
+				msg->ip_addr_from = ip_addr;
+
+				eth_reply(frame, sizeof(arp_message_t));
+			}
+		}
+	}
+}
+
+//
+//END OF ARP
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
