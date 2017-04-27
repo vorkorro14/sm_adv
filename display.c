4b06dc08bf2272d45b2434a38fe30f84189103fb
ar0usel <unbeat@bk.ru>
2017-04-27 17:37:28 +0300

    Update display.c
    display_print() rename to lcd_print();
    new functions:
    lcd_clean();
    lcd_init();

--- display.c
+++ display.c
@@ -9,6 +9,7 @@
 #define RS 2        //RS=PD2 - сигнал управления ЖКИ
 #define E  3        //E=PD3  - сигнал управления ЖКИ
 #define RUN 0b00011000
+#define CLEAN 0b00000001
 #define OSHIBKA_STOP 0xDED
 
 #define TIME 10.0    //Константа временной задержки для ЖКИ
@@ -22,22 +23,24 @@ void pause (unsigned int a)
     asm("nop");
     };
 }
-
+void port_init()
+{
+    DDRD = 0xfc;	//Инициализация portD
+    PORTD = 0x00;
+}
 //Программа передачи команд в ЖКИ
 void lcd_com (unsigned char lcd)
-{ unsigned char temp;
- 
-  temp=(lcd&~(1<<RS))|(1<<E);		//RS=0 – это команда
-  PORTD=temp;	                        //Выводим на portD старшую тетраду команды, сигналы RS, E 
-   asm("nop");	                        //Небольшая задержка в 1 такт МК, для стабилизации
-  PORTD=temp&~(1<<E);	                //Сигнал записи команды
- 
-  temp=((lcd*16)&~(1<<RS))|(1<<E);	//RS=0 – это команда 
-  PORTD=temp;	                        //Выводим на portD младшую тетраду команды, сигналы RS, E 
-   asm("nop");	                        //Небольшая задержка в 1 такт МК, для стабилизации 
-  PORTD=temp&~(1<<E);	                //Сигнал записи команды 
- 
-  pause (10*TIME);	                //Пауза для выполнения команды
+{   
+    unsigned char temp;
+    temp=(lcd&~(1<<RS))|(1<<E);		//RS=0 – это команда
+    PORTD=temp;	                        //Выводим на portD старшую тетраду команды, сигналы RS, E 
+    asm("nop");	                        //Небольшая задержка в 1 такт МК, для стабилизации
+    PORTD=temp&~(1<<E);	                //Сигнал записи команды
+    temp=((lcd*16)&~(1<<RS))|(1<<E);	//RS=0 – это команда 
+    PORTD=temp;	                        //Выводим на portD младшую тетраду команды, сигналы RS, E 
+    asm("nop");	                        //Небольшая задержка в 1 такт МК, для стабилизации 
+    PORTD=temp&~(1<<E);	                //Сигнал записи команды 
+    pause (10*TIME);	                //Пауза для выполнения команды
 }
  void lcd_com_inf (unsigned char lcd)
  {
@@ -46,34 +49,34 @@ void lcd_com (unsigned char lcd)
          lcd_com(lcd);
      }
  }
-//Программа записи данных в ЖКИ
+
 void lcd_dat (unsigned char lcd)
-{ unsigned char temp;
+{ 
+    unsigned char temp;
  
-  temp=(lcd|(1<<RS))|(1<<E);		//RS=1 – это данные 
-  PORTD=temp;	                        //Выводим на portD старшую тетраду данных, сигналы RS, E   
-   asm("nop");		                //Небольшая задержка в 1 такт МК, для стабилизации 
-  PORTD=temp&~(1<<E);	                //Сигнал записи данных 
+    temp=(lcd|(1<<RS))|(1<<E);		//RS=1 – это данные 
+    PORTD=temp;	                        //Выводим на portD старшую тетраду данных, сигналы RS, E   
+    asm("nop");		                //Небольшая задержка в 1 такт МК, для стабилизации 
+    PORTD=temp&~(1<<E);	                //Сигнал записи данных 
  
-  temp=((lcd*16)|(1<<RS))|(1<<E);	//RS=1 – это данные 
-  PORTD=temp;	                        //Выводим на portD младшую тетраду данных, сигналы RS, E   
-   asm("nop");	                        //Небольшая задержка в 1 такт МК, для стабилизации 
-  PORTD=temp&~(1<<E);	                //Сигнал записи данных 
-  pause(0.1*TIME);	                        //Пауза для вывода данных
+    temp=((lcd*16)|(1<<RS))|(1<<E);	//RS=1 – это данные 
+    PORTD=temp;	                        //Выводим на portD младшую тетраду данных, сигналы RS, E   
+    asm("nop");	                        //Небольшая задержка в 1 такт МК, для стабилизации 
+    PORTD=temp&~(1<<E);	                //Сигнал записи данных 
+    pause(0.1*TIME);	                        //Пауза для вывода данных
 }
  
 //Программа иниализации ЖКИ
 void lcd_init (void)
 {
- DDRD = 0xfc;	//Инициализация portD
- PORTD = 0x00;
- pause(1000); 
- lcd_com(0x2c);		//4-проводный интерфейс, 5x8 размер символа
- pause(100*TIME);
- lcd_com(0x0c);		//Показать изображение, курсор не показывать
- pause(100*TIME);
- lcd_com(0x01);		//Очистить DDRAM и установить курсор на 0x00
- pause (100*TIME);
+    port_init();
+    pause(1000);	
+    lcd_com(0x2c);		//4-проводный интерфейс, 5x8 размер символа
+    pause(100*TIME);
+    lcd_com(0x0c);		//Показать изображение, курсор не показывать
+    pause(100*TIME);
+    lcd_com(0x01);		//Очистить DDRAM и установить курсор на 0x00
+    pause (100*TIME);
 }
 
 void lcd_data16(char* string)
@@ -94,7 +97,7 @@ void lcd_data16r(char* string)
 
     }  
 }
- void display_print(char* string) 
+ void lcd_print(char* string) 
 {
     
     int counter = 0;
@@ -104,9 +107,6 @@ void lcd_data16r(char* string)
     lcd_com_inf(RUN);
 }
 
-
-#define port(p) PORT##p
-#define ddr(p) DDR##p
 void next_string()
 {
     int i = 0;
@@ -115,5 +115,7 @@ void next_string()
         lcd_com(0b00010000);
     }
 }
-
-
+void lcd_clean()
+{
+    lcd_com(CLEAN);
+}
