83d47fa81014fc58740287c408030f12b4de78eb
ar0usel <unbeat@bk.ru>
2017-04-27 17:17:11 +0300

    Update display.c
    display_print(char* string) - печать строки на экран(до 32 символов)

--- display.c
+++ display.c
@@ -8,8 +8,10 @@
  
 #define RS 2        //RS=PD2 - сигнал управления ЖКИ
 #define E  3        //E=PD3  - сигнал управления ЖКИ
- 
-#define TIME 10	    //Константа временной задержки для ЖКИ
+#define RUN 0b00011000
+#define OSHIBKA_STOP 0xDED
+
+#define TIME 10.0    //Константа временной задержки для ЖКИ
 		    //Частота тактирование МК - 4Мгц
  
 //Программа формирвоания задержки
@@ -37,7 +39,13 @@ void lcd_com (unsigned char lcd)
  
   pause (10*TIME);	                //Пауза для выполнения команды
 }
- 
+ void lcd_com_inf (unsigned char lcd)
+ {
+     while(1)
+     {
+         lcd_com(lcd);
+     }
+ }
 //Программа записи данных в ЖКИ
 void lcd_dat (unsigned char lcd)
 { unsigned char temp;
@@ -51,13 +59,15 @@ void lcd_dat (unsigned char lcd)
   PORTD=temp;	                        //Выводим на portD младшую тетраду данных, сигналы RS, E   
    asm("nop");	                        //Небольшая задержка в 1 такт МК, для стабилизации 
   PORTD=temp&~(1<<E);	                //Сигнал записи данных 
- 
-  pause(TIME);	                        //Пауза для вывода данных
+  pause(0.1*TIME);	                        //Пауза для вывода данных
 }
  
 //Программа иниализации ЖКИ
 void lcd_init (void)
 {
+ DDRD = 0xfc;	//Инициализация portD
+ PORTD = 0x00;
+ pause(1000); 
  lcd_com(0x2c);		//4-проводный интерфейс, 5x8 размер символа
  pause(100*TIME);
  lcd_com(0x0c);		//Показать изображение, курсор не показывать
@@ -65,47 +75,45 @@ void lcd_init (void)
  lcd_com(0x01);		//Очистить DDRAM и установить курсор на 0x00
  pause (100*TIME);
 }
- void running_string(char* string) 
+
+void lcd_data16(char* string)
 {
-    size_t N = strlen(string); //количество символов
-    int j = 1;
     int i = 0;
-    int k = 0;
-    for (i = 0; i < N; ++i) {
-        for (k = 0; k < j; ++k) {
-            int x = 32 - i + k;
-            if (x >= 0) {
-                lcd_dat(string[i]);
-            }
-        }
-        _delay_ms(50);
-    }
+    for (i ; i < 16; i++)
+    {
+        lcd_dat(string[i]);
+    }  
 }
+void lcd_data16r(char* string)
+{
+    int i = 0;
+    for (i ; i < 16; i++)
+    {
+        lcd_dat(string[i]);
+        lcd_com(RUN);
+
+    }  
+}
+ void display_print(char* string) 
+{
+    
+    int counter = 0;
+    size_t N = strlen(string); //количество символов
+    lcd_data16(string);
+    lcd_data16r(string + 16);
+    lcd_com_inf(RUN);
+}
+
+
 #define port(p) PORT##p
 #define ddr(p) DDR##p
-
-//Основная программа
-int main(void)
-{    
-DDRD = 0xfc;	//Инициализация portD
-PORTD = 0x00;
-
- 
-pause(1000);	//Задержка, чтобы ЖКИ успел включиться
-lcd_init();	//Инициализация ЖКИ
-char* string = "PLASTMASSOVI MIR POBEDIL ";
-
-int k = 0;
-        for (k = 0; k < strlen(string) + 1; ++k) {
-                lcd_dat(string[k]);
-                
-        }
-            //asm("ldi R17,0b00011000");
-    
-        while(1){
-            lcd_com(0b00011000);
-            lcd_com(0b00010100);
-        }
-    return 0;
+void next_string()
+{
+    int i = 0;
+    for (i; i<40  ;i++)
+    {
+        lcd_com(0b00010000);
+    }
 }
+
 
